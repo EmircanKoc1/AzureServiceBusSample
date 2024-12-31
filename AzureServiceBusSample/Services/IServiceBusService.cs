60baa515425bef39ce2d63c41ce601a702ef05c6
Emircan <emircandv787@gmail.com>
2024-12-31 22:27:59 +0300

    The IServiceBusService has been defined.

---
+++ AzureServiceBusSample/Services/IServiceBusService.cs
@@ -0,0 +1,56 @@
+﻿using System.Runtime.InteropServices;
+
+namespace AzureServiceBusSample.Services;
+
+public interface IServiceBusService
+{
+    Task<bool> CreateQueueAsync(
+        string name,
+        TimeSpan lockDuration,
+        int maxDeliveryCount,
+        long maxQueueSizeMB);
+
+    Task<bool> CreateTopicAsync(
+        string name,
+        long maxTopicSizeMB);
+
+    Task<bool> DeleteQueueAsync(string name);
+    Task<bool> DeleteTopicAsync(string name);
+
+    Task<bool> CreateSubscriptionAsync(
+        string topicName,
+        string subscriptionName,
+        [Optional] string ruleName,
+        [Optional] string filter);
+
+    Task<bool> DeleteSubscriptionAsync(string topicName, string subscriptionName);
+
+
+    Task<IEnumerable<string>> ListQueueNamesAsync();
+
+    Task<IEnumerable<string>> ListTopicNamesAsync();
+
+    Task<bool> SendMessageToQueueAsync(
+        string queueName, 
+        BinaryData message,
+        DateTimeOffset scheduleAt);
+    Task<bool> SendMessageToTopicAsync(
+        string topicName,
+        BinaryData message,
+        DateTimeOffset scheduleAt);
+
+    Task<IEnumerable<BinaryData>> ReceiveSubscriptionAsync(
+        string topicName,
+        string subscriptionName,
+        int messageCount);
+
+    Task<IEnumerable<BinaryData>> ReceiveQueueAsync(
+       string queueName,
+       int messageCount);
+
+
+
+
+
+
+}
